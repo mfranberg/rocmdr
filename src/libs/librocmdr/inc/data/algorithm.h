7721c1e2751757d3a37a1821971e616ff6b3bd3a
Mattias Franberg <mattias.franberg@gmail.com>
2012-04-25 12:04:58 +0200

    Reduced the severe contention between threads caused by generating random numbers using rand().

--- src/libs/librocmdr/inc/data/algorithm.h
+++ src/libs/librocmdr/inc/data/algorithm.h
@@ -10,8 +10,48 @@
 
 #include <algorithm>
 #include <vector>
+#include <tr1/random>
 
-#include "ColumnData.h"
+#include <data/ColumnData.h>
+
+/**
+ * A functor that generates a random integer in the
+ * given range using a Mersenne twister random
+ * generator. I use this instead of rand(), to avoid
+ * the lock used in rand() that causes severe contention
+ * when using multiple threads.
+ */
+class RandomInt
+{
+public:
+	RandomInt(unsigned long seed)
+	:	m_rand( ),
+		m_generator( seed )
+	{
+
+	}
+
+	/**
+	 * Generates a random number between [0, max), i.e.
+	 * non inclusively.
+	 */
+	int operator()(unsigned int max)
+	{
+		return m_rand( m_generator, max );
+	}
+
+private:
+	/**
+	 * Takes a uniform random integer in the given range.
+	 */
+	std::tr1::uniform_int<int> m_rand;
+
+	/**
+	 * Mersenne twister random generator.
+	 */
+	std::tr1::mt19937 m_generator;
+};
+
 
 /**
  * Shuffles the data within each class, effectively removing the
@@ -23,10 +63,10 @@
  * @return The shuffled data.
  */
 template <class T>
-std::vector<T> shuffleColumn(std::vector<T> data, const PhenotypeMapping &phenotypes)
+std::vector<T> shuffleColumn(std::vector<T> data, const PhenotypeMapping &phenotypes, RandomInt &rand)
 {
 	std::vector<unsigned int> positive = phenotypes.getPositiveIndices( );
-	random_shuffle( positive.begin( ), positive.end( ) );
+	std::random_shuffle( positive.begin( ), positive.end( ), rand );
 
 	for(unsigned int i = 1; i < positive.size( ); i++)
 	{
@@ -36,7 +76,7 @@ std::vector<T> shuffleColumn(std::vector<T> data, const PhenotypeMapping &phenot
 	}
 
 	std::vector<unsigned int> negative = phenotypes.getNegativeIndices( );
-	random_shuffle( negative.begin( ), negative.end( ) );
+	std::random_shuffle( negative.begin( ), negative.end( ), rand );
 
 	for(unsigned int i = 1; i < negative.size( ); i++)
 	{
@@ -59,13 +99,14 @@ std::vector<T> shuffleColumn(std::vector<T> data, const PhenotypeMapping &phenot
  * @return The shuffled data.
  */
 template <class T>
-ColumnData<T> shuffleColumnData(ColumnData<T> data, const PhenotypeMapping &phenotypes)
+ColumnData<T> shuffleColumnData(ColumnData<T> &data, const PhenotypeMapping &phenotypes)
 {
 	ColumnData<T> shuffledData;
+	RandomInt rand = RandomInt( random( ) );
 
 	for(unsigned int i = 0; i < data.size( ); i++)
 	{
-		std::vector<T> shuffledColumn = shuffleColumn( data.getColumn( i ), phenotypes );
+		std::vector<T> shuffledColumn = shuffleColumn( data.getColumn( i ), phenotypes, rand );
 		shuffledData.addColumn( shuffledColumn );
 	}
 
