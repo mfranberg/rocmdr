4de79803ad1bc486efd27250302dfe33e0120d2b
Mattias Franberg <mattias.franberg@gmail.com>
2012-09-13 15:06:54 +0200

    Splitting of indices sometimes returned too few indices. Also added cstring header so that it builds in gcc 4.7.0.

--- src/libs/librocmdr/src/util/SplitIndices.cpp
+++ src/libs/librocmdr/src/util/SplitIndices.cpp
@@ -5,14 +5,15 @@
  *      Author: fmattias
  */
 #include <algorithm>
+#include <cstring>
 
 #include <util/SplitIndices.h>
 
 Range
 splitIndices(size_t start, size_t end, size_t index, size_t numParts)
 {
-	unsigned int snpsPerPart = ( end - start + 1 ) / numParts;
-	if( snpsPerPart == 0 )
+	unsigned int snpsPerPart = ( end - start + numParts - 1 ) / numParts;
+	if( snpsPerPart == 0 && start - end > 0 )
 	{
 		snpsPerPart = 1;
 	}
