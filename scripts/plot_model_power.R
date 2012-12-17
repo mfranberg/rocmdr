383fcf60e467a2863db9bb00a91ba6742166728a
Mattias Franberg <mattias.franberg@gmail.com>
2012-12-17 14:26:01 +0100

    Updated script for plotting model power.

--- scripts/plot_model_power.R
+++ scripts/plot_model_power.R
@@ -1,23 +1,25 @@
+library( ggplot2 )
+
 argv = commandArgs( trailingOnly = TRUE )
-if ( length( argv ) != 4 )
+if ( length( argv ) != 5 )
 {
     message( "error: Wrong number of arguments." )
-    message( "Usage: plot_model_power level rocmdr_input glm_input" )
+    message( "Usage: plot_model_power title level rocmdr_input glm_input output_file" )
     quit( )
 }
 
 compute_power = function( x )
 {
-    return( sum( x <= 0.05 ) / length( x ) )
+    return( sum( x <= 0.005 ) / length( x ) )
 }
 
-level = as.numeric( argv[ 1 ] )
-rocmdr_power = read.table( argv[ 2 ], header = F, sep = "\t" )
+level = as.numeric( argv[ 2 ] )
+rocmdr_power = read.table( argv[ 3 ], header = F, sep = "\t" )
 rocmdr_power.agg = aggregate( rocmdr_power$V3, by = list( model = rocmdr_power$V4 ), FUN = compute_power )
 
-glm_power = read.table( argv[ 3 ], header = F, sep = "\t" )
+glm_power = read.table( argv[ 4 ], header = F, sep = "\t" )
 glm_power.agg = aggregate( glm_power$V2, by = list( model = glm_power$V3 ), FUN = compute_power )
-output_file = argv[ 4 ]
+output_file = argv[ 5 ]
 
 power = c( rocmdr_power.agg$x, glm_power.agg$x )
 model = c( rocmdr_power.agg$model, glm_power.agg$model )
@@ -30,5 +32,5 @@ ggplot( power_data, aes( x = model ) ) +
     geom_line( aes( y = power, col = method ) ) +
     scale_x_discrete( "Model (increasing effect size)", limits = c( 0, 1.0 ) ) +
     scale_y_continuous( "Power") +
-    opts( title = "Power for different methods" )
+    opts( title = paste( "Power under", argv[ 1 ], "model" ) )
 dev.off( )
